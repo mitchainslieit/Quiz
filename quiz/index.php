ed4430d9ad4fe2638df54c5dcc90ef55bc0b80f7
Mitch Ainslie Galatcha <35722802+mitchainslieit@users.noreply.github.com>
2018-05-04 20:15:37 +0800

    Add files via upload

---
+++ quiz/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Quiz UI</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+        <link rel="stylesheet" href="quiz.css">
+    </head>
+    <body class="bg-light">
+        <div class="cover">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="p-3 row">
+                                    <div class="p-2 text-center col-md-7 bg-secondary rounded">
+                                        <h3 class="text-light">Questions</h3>
+                                        <ol class="list-group text-left">
+                                            <li class="list-group-item">
+                                                <p>Lorem <span class="border border-dark rounded" id="answerbox"></span> dolor sit amet, consectetur adipiscing elit. Sed ultricies, elit sit amet ultricies congue, mauris metus consectetur neque, vel semper quam sem nec ligula. Suspendisse at lobortis nisi.</p>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, elit sit amet ultricies congue, mauris metus consectetur neque, <span class="border border-dark rounded" id="answerbox"></span> semper quam sem nec ligula. Suspendisse at lobortis nisi.</p>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <p>Lorem ipsum dolor sit amet, <span class="border border-dark rounded" id="answerbox"></span> adipiscing elit. Sed ultricies, elit sit amet ultricies congue, mauris metus consectetur neque, vel semper quam sem nec ligula. Suspendisse at lobortis nisi.</p>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <p>Lorem ipsum dolor sit amet, adipiscing elit. Sed ultricies, elit sit amet ultricies congue, mauris metus consectetur neque, vel <span class="border border-dark rounded" id="answerbox"></span>  quam sem nec ligula. Suspendisse at lobortis nisi.</p>
+                                            </li>
+                                        </ol>
+                                    </div>
+                                    <div class="col-md-2"></div>
+                                    <div class="p-2 text-center col-md-3 bg-secondary rounded">
+                                        <h3 class="text-light">Answers</h3>
+                                        <ul class="list-group">
+                                            <li class="list-group-item">Vivamus</li>
+                                            <li class="list-group-item">convallis</li>
+                                            <li class="list-group-item">condimentum</li>
+                                            <li class="list-group-item">Aliquam</li>
+                                            <li class="list-group-item">faucibus</li>
+                                            <li class="list-group-item">Pellentesque </li>
+                                            <li class="list-group-item">malesuada</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </body>
+</html>
